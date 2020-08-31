633b154dd6f9615fd209f9683c001bd9b5098204
nicabuhat <cabuhat.nicaela@gmail.com>
2020-08-31 23:56:57 +0800

    Add hook for markdown changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMarkdown from 'react-markdown';
+import './app.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let initialText = '# Heading 1 \n';
+    initialText += '## Heading 2 \n';
+    initialText += '### Heading 3 \n';
+    initialText += '#### Heading 4 \n';
+    initialText += '- list item 1\n';
+    initialText += '- list item 2\n';
+    initialText += '- list item 3\n';
+    initialText += '\n> This is a blockquote\n\n';
+    initialText += `Lorem ipsum dolor sit amet **this is bold text** elit. 
+    Voluptatibus neque nemo dolor pariatur *this is italicized text* aspernatur, 
+    iure adipisci consectetur voluptates. Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+    Deleniti iste incidunt quis eligendi id vel debitis ratione sit dolore aliquid, 
+    asperiores nam eveniet dolorum beatae tempora enim dolor nesciunt a.`;
+    initialText += '\n\n\n **!! remove content to start new !!**';
+
+    const [markdown, setMarkdown] = useState(initialText);
+
+    function handleTextChange(e) {
+        setMarkdown(e.target.value);
+    }
+
+    return (
+        <div className="app">
+            <textarea
+                className="text-area"
+                onChange={handleTextChange}
+                value={markdown}
+            />
+            <ReactMarkdown className="preview-area" source={markdown} />
+        </div>
+    );
 }
 
 export default App;
